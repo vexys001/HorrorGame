36573777e871dc59aad05f30caa06a7acea75dab
YvesMor <yvesmorin001@gmail.com>
2022-04-23 23:59:51 -0400

    Audio Trigger + Extra Organization

---
+++ Assets/Scripts/Triggers/AudioTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    public string TriggerTarget;
+    public AudioClip _clipToPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals(TriggerTarget))
+        {
+            _audioSource.PlayOneShot(_clipToPlay);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 README Test
+
+Creaking floor by Kostas17
+https://freesound.org/people/Kostas17/sounds/536365/
