2a521a91bc12290ec264ae3580808bcaa5668c8e
YvesMor <yvesmorin001@gmail.com>
2022-04-25 21:53:39 -0400

    Added Things done in Salim's Class

---
+++ Assets/SalimHorrorClass/Import/TriggerZone.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerZone : MonoBehaviour 
+{
+	//--- TRIGGER VARIABLES
+	[Header("TRIGGER SETTINGS")]
+	[Tooltip("Target object that is triggered (usually a manager)")]
+	public GameObject targetManager; //Single target object that is triggered (usually a manager)
+	[Tooltip("Is the target triggered only once of multiple times")]
+	public bool  triggerOnce = true;
+	public int  triggerID = 0;
+	private bool  canBeTriggered = true;
+	
+	//--- USER VARIABLES
+	[Header("USER SETTINGS")]
+	[Tooltip("Name of the users triggering the zone")]
+	public string[] userNameList;
+	private bool  userIsUsingTrigger = false; //is the right user entering the trigger zone?
+
+	//--- UTILITY VARIABLES
+	private int i = 0; // for loops
+	
+	//----------------------------------------------
+	//--- TRIGGER FUNCTIONS
+	void  OnTriggerEnter ( Collider user)
+	{
+		if (canBeTriggered)
+		{
+			//check if the user has the right name
+			i = 0; 
+			while (i < userNameList.Length)
+			{
+				if(user.gameObject.name == userNameList[i]){userIsUsingTrigger = true;}
+				i++;
+			}
+			
+			//if so trigger the target manager
+			if(userIsUsingTrigger)
+			{
+				userIsUsingTrigger = false;
+				if(triggerOnce) {canBeTriggered = false;}
+				targetManager.SendMessage("Triggered",triggerID);
+			}
+		}
+	}
+}
---
+++ Assets/SalimHorrorClass/SoundManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource Source01;
+    public AudioSource Source02;
+
+    private AudioSource _currentSource;
+
+    public float FadeTime = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeIn();
+        FadeOutOtherSound();
+    }
+
+    void Triggered(int id)
+    {
+        Debug.Log($"Trigger ID: {id}");
+        if (id == 0)
+        {
+            PlayAudio01();
+        }
+        else if (id == 1)
+        {
+            PlayAudio02();
+        }
+        else PlaySilence();
+    }
+
+    void PlaySilence()
+    {
+        _currentSource = null;
+    }
+
+    void PlayAudio01()
+    {
+        StartFadeIn(Source01);
+    }
+
+    void PlayAudio02()
+    {
+        StartFadeIn(Source02);
+    }
+
+    void StartFadeIn(AudioSource newSound)
+    {
+        if (_currentSource != newSound)
+        {
+            _currentSource = newSound;
+
+            _currentSource.Play();
+            _currentSource.volume = 0;
+        }
+    }
+
+    void FadeIn()
+    {
+        if (_currentSource && _currentSource.volume < 1)
+        {
+            _currentSource.volume = Mathf.MoveTowards(_currentSource.volume, 1, 1 / FadeTime * Time.deltaTime);
+        }
+    }
+
+    void FadeOutOtherSound()
+    {
+        if (_currentSource)
+        {
+            if (_currentSource == Source01)
+            {
+                if (Source02.volume != 0) Source02.volume = Mathf.MoveTowards(Source02.volume, 0, 1 / FadeTime * Time.deltaTime);
+                else if (Source02.isPlaying)
+                {
+                    Source02.Stop();
+                }
+            }
+            else
+            {
+                if (Source01.volume != 0) Source01.volume = Mathf.MoveTowards(Source01.volume, 0, 1 / FadeTime * Time.deltaTime);
+                else if (Source01.isPlaying)
+                {
+                    Source01.Stop();
+                }
+            }
+        }
+    }
+}
