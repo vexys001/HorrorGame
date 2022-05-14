60aad147077241cb55bef673348def6d56f4a266
YvesMor <yvesmorin001@gmail.com>
2022-05-14 11:52:23 -0400

    Cigarette placing

--- Assets/Scripts/Player/AvatarSmoking.cs
+++ Assets/Scripts/Player/AvatarSmoking.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class AvatarSmoking : MonoBehaviour
 {
+    public GameObject CigaretteGO;
+    public Transform ItemHolder;
+    private bool _isSmoking;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _isSmoking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) && !_isSmoking)
         {
+            //Start smoking
             SmokingManager.Instance.StartSmoking();
+            _isSmoking = true;
+
+            //Placing the cigarette
+            CigaretteGO.transform.parent = ItemHolder;
+
+            CigaretteGO.transform.localPosition = Vector3.zero;
+            CigaretteGO.transform.localRotation = Quaternion.Euler(0,0, 180);
+            //CigaretteGO.transform.localRotation = Quaternion.identity;
         }
     }
 }
