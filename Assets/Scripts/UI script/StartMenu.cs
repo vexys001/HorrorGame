47a1e26ecd5dde4c58cc42b26097db5bcbad1e8e
Ahreum999 <ahreum999@gmail.com>
2022-05-14 22:13:10 -0400

    scene transition done

--- Assets/Scripts/UI script/StartMenu.cs
+++ Assets/Scripts/UI script/StartMenu.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Animator _imageTransition;
+    [SerializeField]
+    private float _transTime = 1f;
+
     public void LoadScene()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        
+    }
+
+    IEnumerator LoadLevel(int level)
+    {
+        level = 3;
+        _imageTransition.SetTrigger("start");
+        yield return new WaitForSeconds(_transTime);
+        SceneManager.LoadScene(level);
     }
 
     public void QuitScene()
